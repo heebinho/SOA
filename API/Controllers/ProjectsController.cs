f56fde5970ace9a9f73b67247347d78f9527c70b
Renato Heeb <renatoheeb@gmail.com>
2018-11-14 20:29:52 +0100

    Core API Swashbuckle and Versioning

---
+++ API/Controllers/ProjectsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        // GET: api/Projects
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "project1", "project2" };
+        }
+
+        // GET: api/Projects/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Projects
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Projects/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        /// <summary>
+        /// Delete a project.
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
